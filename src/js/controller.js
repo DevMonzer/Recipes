790c8b1ae4be64904200da4aceb9f92416f897f5
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-14 16:50:59 +0300

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,12 +118,12 @@ const showRecipe = async function () {
     <h2 class="heading--2">How to cook it</h2>
     <p class="recipe__directions-text">
       This recipe was carefully designed and tested by
-      <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+      <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
       directions at their website.
     </p>
     <a
       class="btn--small recipe__btn"
-      href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+      href="${recipe.sourceUrl}"
       target="_blank"
     >
       <span>Directions</span>
@@ -132,6 +132,9 @@ const showRecipe = async function () {
       </svg>
     </a>
   </div>`;
+
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     alert('Error: ' + err.message);
   }

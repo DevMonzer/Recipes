d62f6be11b6ff971b9133dad6dc1db56c62ec28d
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-14 19:20:15 +0300

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,7 @@
 import icons from 'url:../../img/icons.svg';
 
+import { Fraction } from 'fractional';
+
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
@@ -93,7 +95,9 @@ class RecipeView {
           <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
           </svg>
-          <div class="recipe__quantity">${ing.quantity}</div>
+          <div class="recipe__quantity">${new Fraction(
+            ing.quantity
+          ).toString()}</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
             ${ing.description}

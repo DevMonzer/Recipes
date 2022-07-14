c20a4d46161b9433ff0e53ac5df7d831ce00e07b
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-14 16:46:00 +0300

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -40,9 +40,9 @@ const showRecipe = async function () {
     // 2) Rendering a Recipe
     const markup = `
     <figure class="recipe__fig">
-    <img src="src/img/test-1.jpg" alt="Tomato" class="recipe__img" />
+    <img src="${recipe.image}" alt="${recipe.title}" />
     <h1 class="recipe__title">
-      <span>Pasta with tomato cream sauce</span>
+      <span>${recipe.title}</span>
     </h1>
   </figure>
 
@@ -51,14 +51,14 @@ const showRecipe = async function () {
       <svg class="recipe__info-icon">
         <use href="src/img/icons.svg#icon-clock"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">45</span>
+      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
       <span class="recipe__info-text">minutes</span>
     </div>
     <div class="recipe__info">
       <svg class="recipe__info-icon">
         <use href="src/img/icons.svg#icon-users"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--people">4</span>
+      <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
       <span class="recipe__info-text">servings</span>
 
       <div class="recipe__info-buttons">

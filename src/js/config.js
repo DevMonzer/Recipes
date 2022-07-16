52e91dcbb4a8ac7e6aeb6f1e8cac6d204df77e1d
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-16 10:33:09 +0300

    update config, helper and controller

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,2 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIEMOUT_SEC = 10;
--- src/js/helper.js
+++ src/js/helper.js
@@ -1,11 +1,22 @@
+import { TIEMOUT_SEC } from './config.js';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJSON = async function (url) {
   try {
-    const res = await fetch(url);
+    const res = await Promise.race([fetch(url), timeout(TIEMOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     return data;
   } catch (err) {
-    console.log(err);
+    // Throw the error to the function that is calling this function inside the model file
+    throw err;
   }
 };

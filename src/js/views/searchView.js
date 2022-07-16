ef48ce65bc9a1f0352658a41467a64f3f606c63d
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-16 16:41:02 +0300

    Update searchView.js

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,14 @@ class SearchView {
     const query = this.#parentEl.querySelector('.search__field').value;
     return query;
   }
+
+  // Adding an event listener for the search field button
+  addHandlerSearch(handler) {
+    this.#parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
 }
 
 export default new SearchView();

f9f327928da737867c38a4797ef1ed9f3bbb022c
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-18 18:37:21 +0300

    Update searchView.js

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,16 +1,19 @@
 class SearchView {
   _parentEl = document.querySelector('.search');
 
+  // Getting the search field value
   getQuery() {
     const query = this._parentEl.querySelector('.search__field').value;
     this._clearInput();
     return query;
   }
 
+  // Clear the input search field
   _clearInput() {
     this._parentEl.querySelector('.search__field').value = '';
   }
 
+  // Handle search input submit
   addHandlerSearch(handler) {
     this._parentEl.addEventListener('submit', function (e) {
       e.preventDefault();

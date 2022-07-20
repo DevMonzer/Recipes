0b5f0a01369a68f4c092740264771786985dfe58
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-20 10:10:54 +0300

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -80,8 +80,8 @@ export const loadSearchResults = async function (query) {
 export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
 
-  const start = (page - 1) * state.search.resultsPerPage; // 0
-  const end = page * state.search.resultsPerPage; // 9
+  const start = (page - 1) * state.search.resultsPerPage; // 0 ( page - 1 times the number of results we want per page )
+  const end = page * state.search.resultsPerPage; // 9 ( page time the number of results we want per page )
 
   return state.search.results.slice(start, end);
 };

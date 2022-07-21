56b95e2d886d986c6ce56926e0742de7890a43b2
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-21 10:13:33 +0300

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -36,6 +36,7 @@ export const loadRecipe = async function (id) {
     const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
     state.recipe = createRecipeObject(data);
 
+    // If the recipe doesn't exist in the bookmarks then mark it
     if (state.bookmarks.some(bookmark => bookmark.id === id))
       state.recipe.bookmarked = true;
     else state.recipe.bookmarked = false;
